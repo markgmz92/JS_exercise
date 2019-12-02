fab28ecffac6b5dcd8ad360e2816cc628b3c472f
markgmz92 <markgmz92@gmail.com>
2019-12-02 12:02:55 +0800

    Added a new solution

--- prime_number/script.js
+++ prime_number/script.js
@@ -1,20 +1,60 @@
-function isPrime(num) {
-    for (var i = 2; i < num; i++) {
-        if (num % i === 0) {
-            return false;
-        }
-    }
-    return true;
-}
+//Solution 1
 
-function display(n) {
-    var arr = [2];
-    for ( var i = 3; i < n; i+=2 ) {
-        if ( isPrime(i) ) {
-            arr.push(i);
-        }
-    }
-    console.log(arr);
-}
+// function isPrime(num) {
+//     for (var i = 2; i < num; i++) {
+//         if (num % i === 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
 
-display(50);
+// function display(n) {
+//     var arr = [2];
+//     for ( var i = 3; i < n; i+=2 ) {
+//         if ( isPrime(i) ) {
+//             arr.push(i);
+//         }
+//     }
+//     console.log(arr);
+// }
+
+// display(50);
+
+// Solution 2
+// function isPrime(x) {
+//     d = x -1;
+//     while (d > 1 ){
+//         if ((x % d == 0)) return false;
+//         d--;
+//     }
+//     return true;
+// }
+
+// function display(n) {
+//     var arr = [2];
+//     for ( var i = 3; i < n; i+= 2) {
+//         if ( isPrime() ) {
+//             arr.push(i);
+//         }
+//     }
+//     console.log(arr);
+// }
+
+// display(50);
+
+// Solution 3 (To determine whether a number is a prime)
+// function isPrime(x) {
+//     d = x -1;
+//     while (d > 1 ){
+//         if ((x % d == 0)) return false;
+//         d--;
+//     }
+//     return true;
+// }
+
+// if (isPrime(41)) {
+//     alert('Prime');
+// } else {
+//     alert('Not Prime');
+// }
